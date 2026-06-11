a375624745ea431e35c7e801b4abd2fa8c447425
osogato180 <osogato180@gmail.com>
2026-06-10 21:28:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+
 app = Flask(__name__)
+
+productos = [
+    {"id": 1, "nombre": "Arroz", "precio": 4.50},
+    {"id": 2, "nombre": "Azúcar", "precio": 3.80},
+    {"id": 3, "nombre": "Aceite", "precio": 9.50}
+]
 
 @app.route("/")
 def home():
-    return "Servicio activo - Cloud Computing G1"
+    return render_template("index.html")
 
-app.run(host="0.0.0.0", port=5000)
+@app.route("/api/productos")
+def get_productos():
+    return jsonify(productos)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
